6efe1efcbb2e1493579cd0dea5685c7e52967e22
Aleksandr Sharifullin <sharifullin1999@gmail.com>
2022-10-11 02:06:19 +0600

    Update API class: add historical data reader and more flexibility with .toml config

--- perpetual_futures/connector/api.py
+++ perpetual_futures/connector/api.py
@@ -1,15 +1,11 @@
 import json
 import requests
 import toml
+from copy import deepcopy
 
 
-def _check_request_result_code(result: requests.request):
-    answer = None
-    if result.status_code == 200:
-        answer = json.loads(result.text)
-    else:
-        answer = 'Error ' + str(result.status_code)
-    return answer
+def get_available_apis():
+    print('Currently available: FTX, Binance')
 
 
 class API:
@@ -18,17 +14,63 @@ class API:
         self.exchange_source = exchange_source
         self.config = toml.load(self.config_path)[self.exchange_source]
 
-    def get_all_available_tickers(self):
-        method = self.config['futures_list']['method']
-        query = self.config['head'] + self.config['futures_list']['query']
-        result = requests.request(method, url=query)
-        answer = _check_request_result_code(result)
+    @staticmethod
+    def _check_request_result_code(result: requests.request):
+        answer = None
+        if result.status_code == 200:
+            answer = json.loads(result.text)
+        else:
+            answer = 'Error ' + str(result.status_code)
         return answer
 
-    def get_historical_data(self, candle_interval, start_time, end_time):
+    def get_all_available_tickers(self):
+        """
+        Return all available futures with current price info
+        Returns
+        -------
+        any(string, list, dict)
+            answer of request with data or error-message
+        """
+        method = self.config['futures_list']['method']
+        request_address = self.config['head'] + self.config['futures_list']['request']['address']
+        result = requests.request(method, url=request_address)
+        answer = API._check_request_result_code(result)
+        return answer
+
+    def get_historical_data(self, **parameters):
+        """
+        Return historical market data from API using predefined parameters (see the parameters in the official
+        documentation of the platform you are interested in; currently, Binance and FTX are available).\n
+        Parameters of API request to get historical prices data are following:\n
+            > ticker -- name of ticker for example BTCUSDT, BTC-PERP etc.\n
+            > candle_interval -- time interval of one candle, defined by open, high, low, close prices and volume\n
+            > start_time -- start time of period of interest\n
+            > end_time -- end time of period of interest\n
+
+        Parameters
+        ----------
+        parameters : **kwargs
+            parameters of API request which should be provided to get historical data
+
+        Returns
+        -------
+        any(string, list, dict)
+            answer of request with data or error-message
+        """
         method = self.config['historical_data']['method']
-        query = self.config['historical_data']['query']
-        parameters = self.config['historical_data']['parameters']
-        result = requests.request(method, url=query, params=parameters)
-        answer = _check_request_result_code(result)
+        request_body = self.config['historical_data']['request']
+        # parameters = self.config['historical_data']['parameters']
+        parameters_mapping = self.config['historical_data']['parameters_mapping']
+
+        request_address = self.config['head'] + request_body['address']
+        old_parameters_keys = deepcopy(list(parameters.keys()))
+        for k_old in old_parameters_keys:
+            parameters[parameters_mapping[k_old]] = parameters.pop(k_old)
+        if all(('replaceable' in request_body.keys(), 'replaceable' in parameters.keys())):
+            replaceable = request_body['replaceable']
+            request_address = request_address.replace(replaceable, parameters['replaceable'])
+            parameters.pop('replaceable')
+
+        result = requests.request(method, url=request_address, params=parameters)
+        answer = API._check_request_result_code(result)
         return answer
