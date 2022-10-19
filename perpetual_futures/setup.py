0807e34321f3387c19ff0b20b272e7a085c2898e
Aleksandr Sharifullin <sharifullin1999@gmail.com>
2022-10-20 01:24:26 +0600

    Add setup.py for main package

---
+++ perpetual_futures/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='perpetual_futures',
+    version='0.0.1',
+    description='This package represents a toolbox for working with perpetual futures traded on CEX and DEX\n' +
+    'The provided toolbox allows to get data, store them and analyze with prebuilt trade strategy.',
+    url='git@github.com:Ingenieur-Aleksandr/perpetual_futures.git',
+    author='Aleksandr Sharifullin',
+    author_email='sharifullin1999@gmail.com',
+    license='MIT',
+    packages=['perpetual_futures'],
+    zip_safe=False
+)
